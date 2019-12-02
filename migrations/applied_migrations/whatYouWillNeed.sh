e436ab566949d355563bf59078ba05950e869472
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-12-02 10:55:38 +0000

    PODS-3698 Charge F WYWN page

---
+++ migrations/applied_migrations/whatYouWillNeed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration whatYouWillNeed"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        /whatYouWillNeed                       controllers.whatYouWillNeedController.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "whatYouWillNeed.title = whatYouWillNeed" >> ../conf/messages.en
+echo "whatYouWillNeed.heading = whatYouWillNeed" >> ../conf/messages.en
+
+echo "Migration whatYouWillNeed completed"
