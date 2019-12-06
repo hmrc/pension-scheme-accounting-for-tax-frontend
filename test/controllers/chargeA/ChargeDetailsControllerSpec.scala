29972fc2a570c8f8434fbaefe7f13342b26de7c9
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2019-12-06 07:40:19 +0000

    PODS-3692 More

---
+++ test/controllers/chargeA/ChargeDetailsControllerSpec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.chargeF
+
+import java.time.LocalDate
+
+import behaviours.ControllerBehaviours
+import controllers.base.ControllerSpecBase
+import data.SampleData
+import forms.chargeF.ChargeDetailsFormProvider
+import matchers.JsonMatchers
+import models.chargeF.ChargeDetails
+import models.{GenericViewModel, NormalMode}
+import pages.ChargeDetailsPage
+import play.api.data.Form
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.viewmodels.{DateInput, NunjucksSupport}
+
+class ChargeDetailsControllerSpec extends ControllerSpecBase with NunjucksSupport with JsonMatchers with ControllerBehaviours {
+  private val templateToBeRendered = "chargeF/chargeDetails.njk"
+  private val dynamicErrorMsg: String = "The date the scheme was de-registered must be between 1 April 2020 and 30 June 2020"
+  private val form = new ChargeDetailsFormProvider()(dynamicErrorMsg)
+  private def chargeDetailsGetRoute: String = controllers.chargeF.routes.ChargeDetailsController.onPageLoad(NormalMode, SampleData.srn).url
+  private def chargeDetailsPostRoute: String = controllers.chargeF.routes.ChargeDetailsController.onSubmit(NormalMode, SampleData.srn).url
+
+  private val valuesValid: Map[String, Seq[String]] = Map(
+    "deregistrationDate.day" -> Seq("3"),
+    "deregistrationDate.month" -> Seq("4"),
+    "deregistrationDate.year" -> Seq("2020"),
+    "amountTaxDue" -> Seq("33.44")
+  )
+
+  private val valuesInvalid: Map[String, Seq[String]] = Map(
+    "deregistrationDate.day" -> Seq("32"),
+    "deregistrationDate.month" -> Seq("13"),
+    "deregistrationDate.year" -> Seq("2003"),
+    "amountTaxDue" -> Seq("33.44")
+  )
+
+  private val jsonToPassToTemplate:Form[ChargeDetails]=>JsObject = form => Json.obj(
+    "form" -> form,
+    "viewModel" -> GenericViewModel(
+      submitUrl = controllers.chargeF.routes.ChargeDetailsController.onSubmit(NormalMode, SampleData.srn).url,
+      returnUrl = frontendAppConfig.managePensionsSchemeSummaryUrl.format(SampleData.srn),
+      schemeName = SampleData.schemeName),
+    "date" -> DateInput.localDate(form("deregistrationDate"))
+  )
+
+  "ChargeDetails Controller" must {
+    behave like controllerWithGET(
+      httpPath = chargeDetailsGetRoute,
+      page = ChargeDetailsPage,
+      data = SampleData.chargeDetails,
+      form = form,
+      templateToBeRendered = templateToBeRendered,
+      jsonToPassToTemplate = jsonToPassToTemplate
+    )
+
+    behave like controllerWithPOST(
+      httpPath = chargeDetailsPostRoute,
+      page = ChargeDetailsPage,
+      data = SampleData.chargeDetails,
+      form = form,
+      templateToBeRendered = templateToBeRendered,
+      requestValuesValid = valuesValid,
+      requestValuesInvalid = valuesInvalid
+    )
+  }
+}
---
+++ test/controllers/chargeA/WhatYouWillNeedControllerSpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.chargeF
+
+import behaviours.ControllerBehaviours
+import controllers.base.ControllerSpecBase
+import data.SampleData
+import matchers.JsonMatchers
+import pages.chargeF.WhatYouWillNeedPage
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.viewmodels.NunjucksSupport
+
+class WhatYouWillNeedControllerSpec extends ControllerSpecBase with NunjucksSupport with JsonMatchers with ControllerBehaviours {
+  private val templateToBeRendered = "chargeF/whatYouWillNeed.njk"
+  private def httpGETRoute: String = controllers.chargeF.routes.WhatYouWillNeedController.onPageLoad(SampleData.srn).url
+
+  private val jsonToPassToTemplate:JsObject = Json.obj(
+    fields = "schemeName" -> SampleData.schemeName, "nextPage" -> SampleData.dummyCall.url)
+
+  "whatYouWillNeed Controller" must {
+    behave like controllerWithGET(
+      httpPath = httpGETRoute,
+      page = WhatYouWillNeedPage,
+      templateToBeRendered = templateToBeRendered,
+      jsonToPassToTemplate = jsonToPassToTemplate
+    )
+  }
+}
