fcb71dfadebdfdbee50fa1f6f7f88474f6badfc1
Vincent Vinnicombe <->
2019-12-12 13:05:27 +0000

    PODS-3841: adding correct save to scaffold

--- .g8/questionPage/app/controllers/$className$Controller.scala
+++ .g8/questionPage/app/controllers/$className$Controller.scala
@@ -74,7 +74,7 @@ class $className$Controller @Inject()(override val messagesApi: MessagesApi,
           value =>
             for {
               updatedAnswers <- Future.fromTry(request.userAnswers.set($className$Page, value))
-              _ <- sessionRepository.set(updatedAnswers)
+              _ <- userAnswersCacheConnector.save(request.internalId, updatedAnswers.data)
             } yield Redirect(navigator.nextPage($className$Page, mode, updatedAnswers))
         )
       }
