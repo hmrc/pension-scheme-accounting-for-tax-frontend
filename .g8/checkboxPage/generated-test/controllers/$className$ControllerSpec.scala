59fa413ca10140f805fb55fbce5df560750f193c
Vincent Vinnicombe <->
2019-12-12 23:10:18 +0000

    PODS-3841: Updating imports

--- .g8/checkboxPage/generated-test/controllers/$className$ControllerSpec.scala
+++ .g8/checkboxPage/generated-test/controllers/$className$ControllerSpec.scala
@@ -11,12 +11,13 @@ import org.mockito.Mockito.{times, verify, when}
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatest.{OptionValues, TryValues}
 import play.api.inject.bind
-import play.api.libs.json.{JsObject, JsString, Json}
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.twirl.api.Html
 import data.SampleData._
+import pages.$className$Page
 import uk.gov.hmrc.viewmodels.NunjucksSupport
 
 import scala.concurrent.Future
