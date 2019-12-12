8ea389545f7ab0bc7a7b54516e273fcb16d87c32
Vincent Vinnicombe <->
2019-12-12 22:58:55 +0000

    PODS-3841: Updating spec scaffold

--- .g8/checkboxPage/generated-test/controllers/$className$ControllerSpec.scala
+++ .g8/checkboxPage/generated-test/controllers/$className$ControllerSpec.scala
@@ -3,8 +3,7 @@ package controllers
 import config.FrontendAppConfig
 import controllers.base.ControllerSpecBase
 import forms.$className$FormProvider
-import matchers.JsonMatchers
-import models.{NormalMode, GenericViewModel, UserAnswers}
+import models.{$className$, GenericViewModel, NormalMode, UserAnswers}
 import org.mockito.ArgumentCaptor
 import org.mockito.Matchers.any
 import org.mockito.Mockito.{times, verify, when}
@@ -15,13 +14,11 @@ import play.api.mvc.Call
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.twirl.api.Html
-import data.SampleData._
-import play.api.data.Form
 import uk.gov.hmrc.viewmodels.NunjucksSupport
 
 import scala.concurrent.Future
 
-class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar with NunjucksSupport with JsonMatchers {
+class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar with NunjucksSupport with JsonMatchers with OptionValues with TryValues {
   val mockAppConfig: FrontendAppConfig = mock[FrontendAppConfig]
   def onwardRoute = Call("GET", "/foo")
 
@@ -33,8 +30,10 @@ class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar wit
 
   val viewModel = GenericViewModel(
     submitUrl = $className;format="decap"$SubmitRoute,
-    returnUrl = onwardRoute.url,
-    schemeName = schemeName)
+  returnUrl = onwardRoute.url,
+  schemeName = schemeName)
+
+  val answers: UserAnswers = userAnswersWithSchemeName.set($className$Page, $className.values.toSet).success.value
 
   "$className$ Controller" must {
 
@@ -73,8 +72,11 @@ class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar wit
       when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
       when(mockRenderer.render(any(), any())(any())) thenReturn Future.successful(Html(""))
 
-      val userAnswers = UserAnswers(userAnswersId).set($className$Page, $className$.values.toSet).success.value
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(answers))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
       val request = FakeRequest(GET, $className;format="decap"$Route)
       val templateCaptor = ArgumentCaptor.forClass(classOf[String])
       val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
@@ -113,7 +115,7 @@ class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar wit
 
       val request =
         FakeRequest(POST, $className;format="decap"$Route)
-          .withFormUrlEncodedBody(("value[0]", $className$.values.head.toString))
+      .withFormUrlEncodedBody(("value[0]", $className$.values.head.toString))
 
       val result = route(application, request).value
 
@@ -128,7 +130,11 @@ class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar wit
       when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
       when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
 
-      val application = applicationBuilder(userAnswers = Some(userAnswersWithSchemeName)).build()
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithSchemeName))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
       val request =  FakeRequest(POST, $className;format="decap"$Route).withFormUrlEncodedBody(("value", "invalid value"))
       val boundForm = form.bind(Map("value" -> "invalid value"))
       val templateCaptor = ArgumentCaptor.forClass(classOf[String])
@@ -166,7 +172,7 @@ class $className$ControllerSpec extends ControllerSpecBase with MockitoSugar wit
     }
 
     "redirect to Session Expired for a POST if no existing data is found" in {
-      
+
       val application = applicationBuilder(userAnswers = None).build()
       val request = FakeRequest(POST, $className;format="decap"$Route).withFormUrlEncodedBody(("value[0]", $className$.values.head.toString))
 
