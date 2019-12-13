103a9d488ad8f285109325f86530f0b037c6a0ad
Vincent Vinnicombe <->
2019-12-13 09:49:43 +0000

    PODS-3841: srn in method args

--- .g8/stringPage/app/controllers/$className$Controller.scala
+++ .g8/stringPage/app/controllers/$className$Controller.scala
@@ -31,7 +31,7 @@ class $className$Controller @Inject()(override val messagesApi: MessagesApi,
 
   private val form = formProvider()
 
-  def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onPageLoad(mode: Mode, srn: String): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
       DataRetrievals.retrieveSchemeName { schemeName =>
         val preparedForm = request.userAnswers.get($className$Page) match {
@@ -53,7 +53,7 @@ class $className$Controller @Inject()(override val messagesApi: MessagesApi,
       }
   }
 
-  def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onSubmit(mode: Mode, srn: String): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
       DataRetrievals.retrieveSchemeName { schemeName =>
         form.bindFromRequest().fold(
