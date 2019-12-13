22b64d59c27b5de098906c71390426091cd4dbeb
Vincent Vinnicombe <->
2019-12-13 09:34:37 +0000

    PODS-3841: ctrlr, view, snake update

--- .g8/page/app/controllers/$className$Controller.scala
+++ .g8/page/app/controllers/$className$Controller.scala
@@ -1,26 +1,49 @@
 package controllers
 
+import config.FrontendAppConfig
+import connectors.SchemeDetailsConnector
+import connectors.cache.UserAnswersCacheConnector
 import controllers.actions._
 import javax.inject.Inject
+import models.GenericViewModel
+import navigators.CompoundNavigator
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import renderer.Renderer
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
 
 import scala.concurrent.ExecutionContext
 
-class $className$Controller @Inject()(
-    override val messagesApi: MessagesApi,
-    identify: IdentifierAction,
-    getData: DataRetrievalAction,
-    requireData: DataRequiredAction,
-    val controllerComponents: MessagesControllerComponents,
-    renderer: Renderer
-)(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+class $className$Controller @Inject()(override val messagesApi: MessagesApi,
+                                      identify: IdentifierAction,
+                                      getData: DataRetrievalAction,
+                                      requireData: DataRequiredAction,
+                                      val controllerComponents: MessagesControllerComponents,
+                                      config: FrontendAppConfig,
+                                      renderer: Renderer,
+                                      schemeDetailsConnector: SchemeDetailsConnector,
+                                      userAnswersCacheConnector: UserAnswersCacheConnector,
+                                      navigator: CompoundNavigator
+                                      )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData).async {
+  def onPageLoad(srn: String): Action[AnyContent] = (identify andThen getData andThen requireData).async {
     implicit request =>
-      
-      renderer.render("$className;format="decap"$.njk").map(Ok(_))
+      DataRetrievals.retrieveSchemeName { schemeName =>
+
+        val nextPage = navigator.nextPage($className$Page, NormalMode, ua, srn)
+        val viewModel = GenericViewModel(
+          submitUrl = "",
+          returnUrl = config.managePensionsSchemeSummaryUrl.format(srn),
+          schemeName = schemeName)
+
+        val json = Json.obj(
+          "schemeName" -> schemeName,
+          "nextPage" -> nextPage.url,
+          "viewModel" -> viewModel
+        )
+
+        renderer.render("$className;format="decap"$.njk", json).map(Ok(_))
+    }
   }
 }
