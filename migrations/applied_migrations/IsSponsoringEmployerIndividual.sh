844db59be6647c1ccc10d0f879a78aa6d73f5553
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-01-06 09:59:03 +0000

    PODS-3796 Add question page

---
+++ migrations/applied_migrations/IsSponsoringEmployerIndividual.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+echo ""
+echo "Applying migration IsSponsoringEmployerIndividual"
+
+echo "Adding routes to conf/app.routes"
+
+echo "" >> ../conf/app.routes
+echo "GET        /:srn/new-return/isSponsoringEmployerIndividual                        controllers.chargeC.IsSponsoringEmployerIndividualController.onPageLoad(mode: Mode = NormalMode, srn: String)" >> ../conf/app.routes
+echo "POST       /:srn/new-return/isSponsoringEmployerIndividual                        controllers.chargeC.IsSponsoringEmployerIndividualController.onSubmit(mode: Mode = NormalMode, srn: String)" >> ../conf/app.routes
+
+echo "GET        /:srn/new-return/changeIsSponsoringEmployerIndividual                  controllers.chargeC.IsSponsoringEmployerIndividualController.onPageLoad(mode: Mode = CheckMode, srn: String)" >> ../conf/app.routes
+echo "POST       /:srn/new-return/changeIsSponsoringEmployerIndividual                  controllers.chargeC.IsSponsoringEmployerIndividualController.onSubmit(mode: Mode = CheckMode, srn: String)" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages"
+echo "" >> ../conf/messages.en
+echo "isSponsoringEmployerIndividual.title = isSponsoringEmployerIndividual" >> ../conf/messages.en
+echo "isSponsoringEmployerIndividual.heading = isSponsoringEmployerIndividual" >> ../conf/messages.en
+echo "isSponsoringEmployerIndividual.checkYourAnswersLabel = isSponsoringEmployerIndividual" >> ../conf/messages.en
+echo "isSponsoringEmployerIndividual.error.required = Select yes if isSponsoringEmployerIndividual" >> ../conf/messages.en
+
+echo "Adding to UserAnswersEntryGenerators"
+awk '/trait UserAnswersEntryGenerators/ {\
+    print;\
+    print "";\
+    print "  implicit lazy val arbitraryIsSponsoringEmployerIndividualUserAnswersEntry: Arbitrary[(IsSponsoringEmployerIndividualPage.type, JsValue)] =";\
+    print "    Arbitrary {";\
+    print "      for {";\
+    print "        page  <- arbitrary[IsSponsoringEmployerIndividualPage.type]";\
+    print "        value <- arbitrary[Boolean].map(Json.toJson(_))";\
+    print "      } yield (page, value)";\
+    print "    }";\
+    next }1' ../test/generators/UserAnswersEntryGenerators.scala > tmp && mv tmp ../test/generators/UserAnswersEntryGenerators.scala
+
+echo "Adding to PageGenerators"
+awk '/trait PageGenerators/ {\
+    print;\
+    print "";\
+    print "  implicit lazy val arbitraryIsSponsoringEmployerIndividualPage: Arbitrary[IsSponsoringEmployerIndividualPage.type] =";\
+    print "    Arbitrary(IsSponsoringEmployerIndividualPage)";\
+    next }1' ../test/generators/PageGenerators.scala > tmp && mv tmp ../test/generators/PageGenerators.scala
+
+echo "Adding to UserAnswersGenerator"
+awk '/val generators/ {\
+    print;\
+    print "    arbitrary[(IsSponsoringEmployerIndividualPage.type, JsValue)] ::";\
+    next }1' ../test/generators/UserAnswersGenerator.scala > tmp && mv tmp ../test/generators/UserAnswersGenerator.scala
+
+echo "Adding helper method to CheckYourAnswersHelper"
+awk '/class CheckYourAnswersHelper/ {\
+     print;\
+     print "";\
+     print "  def isSponsoringEmployerIndividual: Option[Row] = userAnswers.get(IsSponsoringEmployerIndividualPage) map {";\
+     print "    answer =>";\
+     print "      Row(";\
+     print "        key     = Key(msg\"isSponsoringEmployerIndividual.checkYourAnswersLabel\", classes = Seq(\"govuk-!-width-one-half\")),";\
+     print "        value   = Value(yesOrNo(answer)),";\
+     print "        actions = List(";\
+     print "          Action(";\
+     print "            content            = msg\"site.edit\",";\
+     print "            href               = controllers.routes.IsSponsoringEmployerIndividualController.onPageLoad(CheckMode, srn).url,";\
+     print "            visuallyHiddenText = Some(msg\"site.edit.hidden\".withArgs(msg\"isSponsoringEmployerIndividual.checkYourAnswersLabel\"))";\
+     print "          )";\
+     print "        )";\
+     print "      )";\
+     print "  }";\
+     next }1' ../app/utils/CheckYourAnswersHelper.scala > tmp && mv tmp ../app/utils/CheckYourAnswersHelper.scala
+
+echo "Migration IsSponsoringEmployerIndividual completed"
---
+++ test/controllers/chargeC/IsSponsoringEmployerIndividualControllerSpec.scala
@@ -0,0 +1,214 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.chargeC
+
+import config.FrontendAppConfig
+import controllers.base.ControllerSpecBase
+import data.SampleData._
+import forms.chargeC.IsSponsoringEmployerIndividualFormProvider
+import matchers.JsonMatchers
+import models.{GenericViewModel, NormalMode, UserAnswers}
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{times, verify, when}
+import org.scalatest.{OptionValues, TryValues}
+import org.scalatestplus.mockito.MockitoSugar
+import pages.chargeC.IsSponsoringEmployerIndividualPage
+import play.api.inject.bind
+import play.api.libs.json.{JsObject, Json}
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import play.twirl.api.Html
+import uk.gov.hmrc.viewmodels.{NunjucksSupport, Radios}
+
+import scala.concurrent.Future
+
+class IsSponsoringEmployerIndividualControllerSpec extends ControllerSpecBase with MockitoSugar with NunjucksSupport with JsonMatchers with OptionValues with TryValues {
+  val mockAppConfig: FrontendAppConfig = mock[FrontendAppConfig]
+
+  def onwardRoute = Call("GET", "/foo")
+
+  val formProvider = new IsSponsoringEmployerIndividualFormProvider()
+  val form = formProvider()
+
+  lazy val isSponsoringEmployerIndividualRoute = routes.IsSponsoringEmployerIndividualController.onPageLoad(NormalMode, srn).url
+  lazy val isSponsoringEmployerIndividualSubmitRoute = routes.IsSponsoringEmployerIndividualController.onSubmit(NormalMode, srn).url
+
+  val viewModel = GenericViewModel(
+    submitUrl = isSponsoringEmployerIndividualSubmitRoute,
+  returnUrl = onwardRoute.url,
+  schemeName = schemeName)
+
+  val answers: UserAnswers = userAnswersWithSchemeName.set(IsSponsoringEmployerIndividualPage, true).success.value
+
+  "IsSponsoringEmployerIndividual Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
+      when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
+
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithSchemeName))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
+      val request = FakeRequest(GET, isSponsoringEmployerIndividualRoute)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      val expectedJson = Json.obj(
+        "form"   -> form,
+        "viewModel" -> viewModel,
+        "radios" -> Radios.yesNo(form("value"))
+      )
+
+      templateCaptor.getValue mustEqual "isSponsoringEmployerIndividual.njk"
+      jsonCaptor.getValue must containJson(expectedJson)
+
+      application.stop()
+    }
+
+    "populate the view correctly on a GET when the question has previously been answered" in {
+      when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
+      when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
+
+      val application = applicationBuilder(userAnswers = Some(answers))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
+      val request = FakeRequest(GET, isSponsoringEmployerIndividualRoute)
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      val filledForm = form.bind(Map("value" -> "true"))
+
+      val expectedJson = Json.obj(
+        "form"   -> filledForm,
+        "viewModel" -> viewModel,
+        "radios" -> Radios.yesNo(filledForm("value"))
+      )
+
+      templateCaptor.getValue mustEqual "isSponsoringEmployerIndividual.njk"
+      jsonCaptor.getValue must containJson(expectedJson)
+
+      application.stop()
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
+      when(mockUserAnswersCacheConnector.save(any(), any())(any(), any())) thenReturn Future.successful(Json.obj())
+      when(mockCompoundNavigator.nextPage(any(), any(), any(), any())).thenReturn(onwardRoute)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithSchemeName))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
+
+      val request =
+        FakeRequest(POST, isSponsoringEmployerIndividualRoute)
+      .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual onwardRoute.url
+
+      application.stop()
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+
+      when(mockAppConfig.managePensionsSchemeSummaryUrl).thenReturn(onwardRoute.url)
+      when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
+
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithSchemeName))
+        .overrides(
+          bind[FrontendAppConfig].toInstance(mockAppConfig)
+        )
+        .build()
+      val request = FakeRequest(POST, isSponsoringEmployerIndividualRoute).withFormUrlEncodedBody(("value", ""))
+      val boundForm = form.bind(Map("value" -> ""))
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+
+      val result = route(application, request).value
+
+      status(result) mustEqual BAD_REQUEST
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      val expectedJson = Json.obj(
+        "form"   -> boundForm,
+        "viewModel" -> viewModel,
+        "radios" -> Radios.yesNo(boundForm("value"))
+      )
+
+      templateCaptor.getValue mustEqual "isSponsoringEmployerIndividual.njk"
+      jsonCaptor.getValue must containJson(expectedJson)
+
+      application.stop()
+    }
+
+    "redirect to Session Expired for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      val request = FakeRequest(GET, isSponsoringEmployerIndividualRoute)
+
+      val result = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+      application.stop()
+    }
+
+    "redirect to Session Expired for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      val request =
+        FakeRequest(POST, isSponsoringEmployerIndividualRoute)
+      .withFormUrlEncodedBody(("value", "true"))
+
+      val result = route(application, request).value
+
+      status(result) mustEqual SEE_OTHER
+
+      redirectLocation(result).value mustEqual controllers.routes.SessionExpiredController.onPageLoad().url
+
+      application.stop()
+    }
+  }
+}
