ddf97e9322a2b62848721164e9172d488b2ebdc6
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-01-14 15:53:29 +0000

    PODS-3867 Correct ref to graph file for countries

--- app/assets/javascripts/aft.js
+++ app/assets/javascripts/aft.js
@@ -3,6 +3,6 @@ GOVUKFrontend.initAll();
 if (document.querySelector('#country-select') != null) {
     openregisterLocationPicker({
         selectElement: document.getElementById('country-select'),
-        url: '/assets/javascript/autocomplete/location-autocomplete-graph.json'
+        url: '/manage-pension-scheme-accounting-for-tax/assets/javascripts/autocomplete/location-autocomplete-graph.json'
     })
 }
