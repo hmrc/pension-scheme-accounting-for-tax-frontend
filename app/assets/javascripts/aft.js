8d88e35575c8eb4d901e834c8b48108a13f47ee8
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-01-14 16:12:37 +0000

    PODS-3867 Add back link functionality

--- app/assets/javascripts/aft.js
+++ app/assets/javascripts/aft.js
@@ -6,3 +6,13 @@ if (document.querySelector('#country-select') != null) {
         url: '/manage-pension-scheme-accounting-for-tax/assets/javascripts/autocomplete/location-autocomplete-graph.json'
     })
 }
+
+var backLink = document.querySelector('.govuk-back-link');
+if(backLink){
+    backLink.addEventListener('click', function(e){
+        e.preventDefault();
+        if (window.history && window.history.back && typeof window.history.back === 'function'){
+            window.history.back();
+        }
+    });
+}
