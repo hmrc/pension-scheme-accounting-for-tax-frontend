0cbdb53d9398565ee364f7546d43f138eb2c2233
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-01-15 11:35:25 +0000

    PODS-3855 Nino changes

--- test/forms/chargeE/MemberDetailsFormProviderSpec.scala
+++ test/forms/chargeE/MemberDetailsFormProviderSpec.scala
@@ -18,13 +18,14 @@ package forms.chargeE
 
 import forms.MemberDetailsFormProvider
 import forms.behaviours.StringFieldBehaviours
+import models.MemberDetails
 import play.api.data.FormError
 
 class MemberDetailsFormProviderSpec extends StringFieldBehaviours {
 
   val form = new MemberDetailsFormProvider()()
 
-  ".firstName" must {
+  "firstName" must {
 
     val fieldName = "firstName"
     val requiredKey = "memberDetails.error.firstName.required"
@@ -51,7 +52,7 @@ class MemberDetailsFormProviderSpec extends StringFieldBehaviours {
     )
   }
 
-  ".lastName" must {
+  "lastName" must {
 
     val fieldName = "lastName"
     val requiredKey = "memberDetails.error.lastName.required"
@@ -76,5 +77,20 @@ class MemberDetailsFormProviderSpec extends StringFieldBehaviours {
       fieldName,
       requiredError = FormError(fieldName, requiredKey)
     )
+  }
+
+  "nino" must {
+    behave like nino(
+      form,
+      fieldName = "nino",
+      requiredKey = "memberDetails.error.nino.required",
+      invalidKey = "memberDetails.error.nino.invalid"
+    )
+
+    "successfully bind when valid NINO with spaces is provided" in {
+      val res = form.bind(Map("firstName" -> "Jane", "lastName" -> "Doe",
+        "nino" -> " a b 0 2 0 2 0 2 a "))
+      res.get mustEqual MemberDetails("Jane", "Doe", "AB020202A")
+    }
   }
 }
