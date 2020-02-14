4f7b459ed9bfcff41356e38261e5673b3f2b01a2
Madhura <madhuranakate@gmail.com>
2020-02-14 13:37:34 +0000

    PODS-3756 Year page

---
+++ app/controllers/YearController.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import audit.{AuditService, StartAFTAuditEvent}
+import config.FrontendAppConfig
+import connectors.cache.UserAnswersCacheConnector
+import controllers.actions._
+import forms.YearFormProvider
+import javax.inject.Inject
+import models.{Years, GenericViewModel, Mode}
+import navigators.CompoundNavigator
+import pages._
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import renderer.Renderer
+import services.{AFTService, AllowAccessService}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import uk.gov.hmrc.viewmodels.NunjucksSupport
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class YearController @Inject()(
+                                      override val messagesApi: MessagesApi,
+                                      userAnswersCacheConnector: UserAnswersCacheConnector,
+                                      navigator: CompoundNavigator,
+                                      identify: IdentifierAction,
+                                      getData: DataRetrievalAction,
+                                      allowAccess: AllowAccessActionProvider,
+                                      requireData: DataRequiredAction,
+                                      formProvider: YearFormProvider,
+                                      val controllerComponents: MessagesControllerComponents,
+                                      renderer: Renderer,
+                                      config: FrontendAppConfig,
+                                      auditService: AuditService,
+                                      aftService: AFTService,
+                                      allowService: AllowAccessService
+                                    )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with NunjucksSupport {
+
+  private val form = formProvider()
+
+  def onPageLoad(mode: Mode, srn: String): Action[AnyContent] = (identify andThen getData(srn) andThen allowAccess(srn) andThen requireData).async {
+    implicit request =>
+
+      DataRetrievals.retrieveSchemeName { schemeName =>
+
+        val preparedForm = request.userAnswers.get(YearPage).fold(form)(form.fill)
+
+        val json = Json.obj(
+          "srn" -> srn,
+          "form" -> preparedForm,
+          "radios" -> Years.radios(preparedForm),
+          "viewModel" -> viewModel(schemeName, mode, srn)
+        )
+
+        renderer.render(template = "year.njk", json).map(Ok(_))
+      }
+  }
+
+  def onSubmit(mode: Mode, srn: String): Action[AnyContent] = (identify andThen getData(srn) andThen requireData).async {
+    implicit request =>
+      DataRetrievals.retrieveSchemeName { schemeName =>
+
+        form.bindFromRequest().fold(
+          formWithErrors => {
+            val json = Json.obj(
+              fields = "srn" -> srn,
+              "form" -> formWithErrors,
+              "radios" -> Years.radios(formWithErrors),
+              "viewModel" -> viewModel(schemeName, mode, srn)
+            )
+            renderer.render(template = "year.njk", json).map(BadRequest(_))
+          },
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(YearPage, value))
+              _ <- userAnswersCacheConnector.save(request.internalId, updatedAnswers.data)
+            } yield Redirect(navigator.nextPage(YearPage, mode, updatedAnswers, srn))
+        )
+      }
+  }
+
+  private def viewModel(schemeName: String, mode: Mode, srn: String): GenericViewModel = {
+    GenericViewModel(
+      submitUrl = routes.YearController.onSubmit(srn).url,
+      returnUrl = config.managePensionsSchemeSummaryUrl.format(srn),
+      schemeName = schemeName
+    )
+  }
+}
---
+++ app/models/Years.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+
+import java.time.LocalDate
+
+import play.api.data.Form
+import play.api.i18n.Messages
+import play.api.libs.json.{JsString, JsValue, Writes}
+import uk.gov.hmrc.viewmodels.Radios
+import uk.gov.hmrc.viewmodels.Text.Literal
+
+sealed trait Years
+
+object Years extends Enumerable.Implicits {
+
+  val currentYear: Int = LocalDate.now().getYear
+
+  def minYear: Int = {
+    val earliestYear = currentYear - 6
+    if(earliestYear > 2020)
+      earliestYear
+    else
+      2020
+  }
+
+  val values: Seq[Year] = (minYear to currentYear).reverse.map(Year(_))
+
+  def radios(form: Form[_])(implicit messages: Messages): Seq[Radios.Item] = {
+
+    Radios(form("value"), values.map(year => Radios.Radio(Literal(year.toString), year.toString)))
+  }
+
+  implicit val enumerable: Enumerable[Years] =
+    Enumerable(values.map(v => v.toString -> v): _*)
+}
+
+case class Year(year: Int) extends Years {
+  override def toString: String = year.toString
+}
+
+object Year {
+  implicit val writes: Writes[Year] = new Writes[Year] {
+    def writes(year: Year): JsValue = JsString(year.toString)
+  }
+}
