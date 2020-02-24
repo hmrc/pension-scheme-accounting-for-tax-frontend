df32eee888ee75d0fe6d721c79afeec5fe186197
Vincent Vinnicombe <->
2020-02-24 13:14:20 +0000

    library-upgrades: Downgraded reactivemongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo"             %%  "play2-reactivemongo"            % "0.20.3-play26",
+    "org.reactivemongo"             %%  "play2-reactivemongo"            % "0.18.3-play26",
     "uk.gov.hmrc"                   %%  "logback-json-logger"            % "4.6.0",
     "uk.gov.hmrc"                   %%  "play-health"                    % "3.14.0-play-26",
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping"  % "1.2.0-play-26",
