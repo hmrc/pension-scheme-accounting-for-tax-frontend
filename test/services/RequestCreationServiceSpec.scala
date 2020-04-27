b5fda4dee44a50ce2284643827947a6b71d363e4
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-04-27 13:35:31 +0100

    PODS-4134 More tests for new service

--- test/services/RequestCreationServiceSpec.scala
+++ test/services/RequestCreationServiceSpec.scala
@@ -30,37 +30,64 @@ import models.SessionAccessData
 import models.SessionData
 import models.UserAnswers
 import models.requests.OptionalDataRequest
-import org.mockito.Mockito.reset
 import org.scalatest.BeforeAndAfterEach
 import play.api.mvc.AnyContent
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
+import org.mockito.Matchers._
+import org.mockito.Mockito.{reset, when}
+import play.api.libs.json.JsObject
+import play.api.libs.json.JsString
+import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class RequestCreationServiceSpec extends SpecBase with MockitoSugar with ScalaFutures with BeforeAndAfterEach {
-  private val aftConnector: AFTConnector = mock[AFTConnector]
-  private val userAnswersCacheConnector: UserAnswersCacheConnector = mock[UserAnswersCacheConnector]
-  private val schemeService: SchemeService = mock[SchemeService]
-  private val minimalPsaConnector: MinimalPsaConnector = mock[MinimalPsaConnector]
+  private val mockAftConnector: AFTConnector = mock[AFTConnector]
+  private val mockUserAnswersCacheConnector: UserAnswersCacheConnector = mock[UserAnswersCacheConnector]
+  private val mockSchemeService: SchemeService = mock[SchemeService]
+  private val mockMinimalPsaConnector: MinimalPsaConnector = mock[MinimalPsaConnector]
   private implicit val req: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("", "")
   private val psaIdInstance = PsaId(psaId)
 
-  private val requestCreationService = new RequestCreationService(aftConnector, userAnswersCacheConnector, schemeService, minimalPsaConnector)
+  private val sessionId = "???"
+  private val internalId = s"$srn$startDate"
+
+  private val jsObject = Json.obj( "one" -> "two")
+  private val optionUA = Some(UserAnswers(jsObject))
+  val name = None
+  private val sessionAccessData = SessionAccessData(version = 1, accessMode = AccessMode.PageAccessModeViewOnly)
+  private val optionSD = Some(SessionData(sessionId, name, sessionAccessData))
+
+  private val requestCreationService = new RequestCreationService(mockAftConnector, mockUserAnswersCacheConnector, mockSchemeService, mockMinimalPsaConnector)
 
   override def beforeEach(): Unit = {
-    reset(aftConnector, userAnswersCacheConnector, schemeService, minimalPsaConnector)
-
+    reset(mockAftConnector, mockUserAnswersCacheConnector, mockSchemeService, mockMinimalPsaConnector)
+    when(mockUserAnswersCacheConnector.fetch(any())(any(),any())).thenReturn(Future.successful(Some(jsObject)))
+    when(mockUserAnswersCacheConnector.getSessionData(any())(any(),any())).thenReturn(Future.successful(optionSD))
   }
 
   "Request creation service" must {
-    "create a request" in {
+    "create a request with  both user answers and session data" in {
       whenReady(requestCreationService.createRequest[AnyContent](psaIdInstance, srn, startDate)) { result =>
-      val expectedName = None
-        val expectedSAD = SessionAccessData(version = 1, accessMode = AccessMode.PageAccessModeViewOnly)
-        val expectedOptionUA = Some(UserAnswers())
-        val expectedOptionSD = Some(SessionData("", expectedName, expectedSAD))
-        val expectedResult = OptionalDataRequest(req, "", psaIdInstance, expectedOptionUA, expectedOptionSD)
+        val expectedResult = OptionalDataRequest(req, internalId, psaIdInstance, optionUA, optionSD)
+        result mustBe expectedResult
+      }
+    }
+
+    "create a request with no user answers but session data" in {
+      when(mockUserAnswersCacheConnector.fetch(any())(any(),any())).thenReturn(Future.successful(None))
+      whenReady(requestCreationService.createRequest[AnyContent](psaIdInstance, srn, startDate)) { result =>
+        val expectedResult = OptionalDataRequest(req, internalId, psaIdInstance, None, optionSD)
+        result mustBe expectedResult
+      }
+    }
+
+    "create a request with no session data" in {
+      when(mockUserAnswersCacheConnector.getSessionData(any())(any(),any())).thenReturn(Future.successful(None))
+      whenReady(requestCreationService.createRequest[AnyContent](psaIdInstance, srn, startDate)) { result =>
+        val expectedResult = OptionalDataRequest(req, internalId, psaIdInstance, None, None)
         result mustBe expectedResult
       }
     }
