c531d3f9a8d34ea635eb09f4b07d4a4167a08b89
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2020-05-22 21:45:47 +0100

    PODS-4269 Tidy

--- app/services/UserAnswersService.scala
+++ app/services/UserAnswersService.scala
@@ -19,7 +19,9 @@ package services
 import com.google.inject.Inject
 import helpers.DeleteChargeHelper
 import models.requests.DataRequest
-import models.{Mode, NormalMode, UserAnswers}
+import models.Mode
+import models.NormalMode
+import models.UserAnswers
 import pages.QuestionPage
 import play.api.libs.json._
 import play.api.mvc.AnyContent
@@ -29,18 +31,19 @@ import scala.util.Try
 class UserAnswersService @Inject()(deleteChargeHelper: DeleteChargeHelper) {
 
   /* Use this set for add/change journeys for a member or scheme based charge */
-  def set[A](page: QuestionPage[A], value: A, mode: Mode, isMemberBased: Boolean = true
-            )(implicit request: DataRequest[AnyContent], writes: Writes[A]): Try[UserAnswers] = {
+  def set[A](page: QuestionPage[A], value: A, mode: Mode, isMemberBased: Boolean = true)(implicit request: DataRequest[AnyContent],
+                                                                                         writes: Writes[A]): Try[UserAnswers] = {
 
-    if(request.isAmendment) { //this IS an amendment
-      if(isMemberBased) { //charge C, D, E or G
+    if (request.isAmendment) { //this IS an amendment
+      if (isMemberBased) { //charge C, D, E or G
 
-        val status: String = if(mode == NormalMode) "New" else getCorrectStatus(page, "Changed", request.userAnswers)
+        val status: String = if (mode == NormalMode) "New" else getCorrectStatus(page, "Changed", request.userAnswers)
 
         request.userAnswers
           .removeWithPath(amendedVersionPath(page))
           .removeWithPath(memberVersionPath(page))
-          .set(page, value).flatMap(_.set(memberStatusPath(page), JsString(status)))
+          .set(page, value)
+          .flatMap(_.set(memberStatusPath(page), JsString(status)))
 
       } else { //charge A, B or F
         val amendedVersionPath: JsPath = JsPath(page.path.path.init ++ List(KeyPathNode("amendedVersion")))
@@ -52,9 +55,10 @@ class UserAnswersService @Inject()(deleteChargeHelper: DeleteChargeHelper) {
   }
 
   /* Use this set for deleting a member based charge */
-  def removeMemberBasedCharge[A](page: QuestionPage[A], totalAmount: UserAnswers => BigDecimal
-                       )(implicit request: DataRequest[AnyContent], writes: Writes[A]): Try[UserAnswers] = {
-    val isRemovable = {
+  def removeMemberBasedCharge[A](page: QuestionPage[A], totalAmount: UserAnswers => BigDecimal)(implicit request: DataRequest[AnyContent],
+                                                                                                writes: Writes[A]): Try[UserAnswers] = {
+    // Can only physically remove if first compile or if member being removed was added in same version
+    def isRemovable = {
       val ua = request.userAnswers
       def previousVersion = ua.get(memberVersionPath(page))
       def prevMemberStatus = ua.get(memberStatusPath(page)).getOrElse(throw MissingMemberStatus)
@@ -63,59 +67,52 @@ class UserAnswersService @Inject()(deleteChargeHelper: DeleteChargeHelper) {
       request.aftVersion == 1 || ((previousVersion.isEmpty || isChangeInSameCompile) && prevMemberStatus.as[String].equals("New"))
     }
 
-    val isLastCharge = deleteChargeHelper.isLastCharge(request.userAnswers)
-
-    def removeOrZeroOutCharge(ua: UserAnswers): UserAnswers =
-      (isLastCharge, isRemovable) match {
-        case (true, _) => deleteChargeHelper.zeroOutLastCharge(ua)
-        case (_, true) => ua.removeWithPath(memberParentPath(page))
-        case _ => ua
-      }
+    def isLastCharge = deleteChargeHelper.isLastCharge(request.userAnswers)
 
     def updateTotalAmount(ua: UserAnswers): Try[UserAnswers] =
       ua.set(totalAmountPath(page), JsNumber(totalAmount(ua)))
 
-    def setAmendmentFlags(ua: UserAnswers): Try[UserAnswers] = {
-        if(isLastCharge || isRemovable) {
-          Try(ua)
-        } else {
-          ua
-            .removeWithPath(amendedVersionPath(page))
-            .removeWithPath(memberVersionPath(page))
-            .set(memberStatusPath(page), JsString("Deleted"))
-        }
-    }
+    val ua = request.userAnswers
 
-    setAmendmentFlags(removeOrZeroOutCharge(request.userAnswers))
-      .flatMap(updateTotalAmount)
+    updateTotalAmount(
+      (isLastCharge, isRemovable) match {
+        case (true, _) => deleteChargeHelper.zeroOutLastCharge(ua)
+        case (_, true) => ua.removeWithPath(memberParentPath(page))
+        case _ =>
+          // Must be removing in amendments where member added in same version
+          ua.removeWithPath(amendedVersionPath(page))
+            .removeWithPath(memberVersionPath(page))
+            .setOrException(memberStatusPath(page), JsString("Deleted"))
+      }
+    )
   }
 
   /* Use this remove for deleting a scheme based charge */
-  def removeSchemeBasedCharge[A](page: QuestionPage[A]
-               )(implicit request: DataRequest[AnyContent]): UserAnswers = {
+  def removeSchemeBasedCharge[A](page: QuestionPage[A])(implicit request: DataRequest[AnyContent]): UserAnswers = {
     val ua: UserAnswers = request.userAnswers
-    if(request.isAmendment) {
+    if (request.isAmendment) {
       val amendedVersionPath: JsPath = page.path \ "amendedVersion"
       deleteChargeHelper.zeroOutCharge(page, ua).removeWithPath(amendedVersionPath)
     } else if (deleteChargeHelper.isLastCharge(ua)) {
-        deleteChargeHelper.zeroOutCharge(page, ua)
+      deleteChargeHelper.zeroOutCharge(page, ua)
     } else {
       ua.removeWithPath(page.path)
     }
   }
 
-  private def getCorrectStatus[A](page: QuestionPage[A], updatedStatus: String, userAnswers: UserAnswers)(implicit request: DataRequest[AnyContent]): String = {
+  private def getCorrectStatus[A](page: QuestionPage[A], updatedStatus: String, userAnswers: UserAnswers)(
+      implicit request: DataRequest[AnyContent]): String = {
 
     val previousVersion = userAnswers.get(memberVersionPath(page))
     val prevMemberStatus = userAnswers.get(memberStatusPath(page)).getOrElse(throw MissingMemberStatus)
 
     val isChangeInSameCompile = previousVersion.nonEmpty && previousVersion.getOrElse(throw MissingVersion).as[Int] == request.aftVersion
 
-   if((previousVersion.isEmpty || isChangeInSameCompile) && prevMemberStatus.as[String].equals("New")) {
+    if ((previousVersion.isEmpty || isChangeInSameCompile) && prevMemberStatus.as[String].equals("New")) {
       "New"
     } else {
-     updatedStatus
-   }
+      updatedStatus
+    }
   }
 
   private def amendedVersionPath[A](page: QuestionPage[A]): JsPath =
