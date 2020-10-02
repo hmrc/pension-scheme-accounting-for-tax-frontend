94d8973d3e16fb94daa5e62f5e980e4f5a4e3039
ashwani1403 <ashwani.kumar@digital.hmrc.gov.uk>
2020-10-02 11:22:34 +0100

    PODS-4763: session time extension functionality

---
+++ app/controllers/KeepAliveController.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import java.time.LocalDate
+
+import config.FrontendAppConfig
+import connectors.cache.UserAnswersCacheConnector
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class KeepAliveController @Inject()(
+    config: FrontendAppConfig,
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    requireData: DataRequiredAction,
+    val controllerComponents: MessagesControllerComponents,
+    userAnswersCacheConnector: UserAnswersCacheConnector
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def keepAlive(srn: String, startDate: Option[LocalDate])(implicit ec: ExecutionContext, hc: HeaderCarrier): Action[AnyContent] = (identify andThen getData(srn, startDate) andThen requireData).async {
+    implicit request =>
+
+      startDate match {
+        case Some(startDt) =>
+          val id = s"$srn$startDt"
+          userAnswersCacheConnector.save(id, request.userAnswers.data).map { _ =>
+            NoContent
+          }
+      }
+
+  }
+
+}
