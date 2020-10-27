565fb3ee91a1e112c0fdfcb4a32d871f8d070327
ashwani1403 <ashwani.kumar@digital.hmrc.gov.uk>
2020-10-27 10:06:18 +0000

    PODS-4778: responsive view for table2

--- test/matchers/JsonMatchers.scala
+++ test/matchers/JsonMatchers.scala
@@ -25,7 +25,6 @@ trait JsonMatchers {
 
     def apply(left: JsObject): MatchResult = {
       val mismatches = json.keys.filter(key => (left \ key) != (json \ key))
-
       MatchResult(
         mismatches.isEmpty,
         s"""$left did not match for key(s) ${mismatches.mkString(", ")}""",
