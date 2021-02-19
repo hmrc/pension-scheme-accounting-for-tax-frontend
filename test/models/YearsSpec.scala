a21e6192216a2d83785bbbb1ff01b59902e2a337
Emma Rawsthorne <27211889+emrawsy@users.noreply.github.com>
2021-02-19 10:40:17 +0000

    PODS-5036 WIP

--- test/models/YearsSpec.scala
+++ test/models/YearsSpec.scala
@@ -16,10 +16,17 @@
 
 package models
 
+import org.mockito.Mockito.when
 import org.scalatest.{FreeSpec, MustMatchers}
+import org.scalatestplus.mockito.MockitoSugar
+import play.api.data.Form
 import play.api.libs.json.{JsString, Json}
+import uk.gov.hmrc.viewmodels.Radios
+import uk.gov.hmrc.viewmodels.Text.Literal
 
-class YearsSpec extends FreeSpec with MustMatchers {
+class YearsSpec extends FreeSpec with MustMatchers with MockitoSugar {
+
+  private val mockForm = mock[Form[String]]
 
   "writes" - {
     "must map correctly to string" in {
@@ -29,4 +36,21 @@ class YearsSpec extends FreeSpec with MustMatchers {
       result mustBe JsString("2020")
     }
   }
+  "AmendYears.values" - {
+    "must return Seq of years in reverse order" in {
+      val years = Seq(1,2,3)
+      val expectedResult = Seq(Year(3), Year(2), Year(1))
+      AmendYears.values(years) mustBe expectedResult
+    }
+  }
+
+  "AmendYears.radios" - {
+    "must return Seq of years as string in reverse order" in {
+      when(mockForm.)
+      val years = Seq(1,2)
+      val expectedResult = Seq(Radios.Radio.apply(Literal("2"), "2"), Radios.Radio(Literal("1"), "1"))
+      AmendYears.radios(mockForm, years) mustBe expectedResult
+    }
+  }
+
 }
