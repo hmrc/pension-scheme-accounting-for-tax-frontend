a28fa0a8750bdbe8e81fefcd76e7176219b0e7c3
Madhura <madhuranakate@gmail.com>
2021-02-23 10:28:19 +0000

    PODS-4954 ADding new controller tests

--- app/models/Years.scala
+++ app/models/Years.scala
@@ -92,7 +92,7 @@ object FSYears extends CommonYears with Enumerable.Implicits {
       Radios.Radio(label = Literal(displayYear.year.toString),
         value = displayYear.year.toString,
         hint = getHint(displayYear),
-        labelClasses = Some(LabelClasses(classes = Seq("govuk-!-font-weight-bold"))))
+        labelClasses = None)
     }
     Radios(form("value"), x)
   }
