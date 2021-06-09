921488ffe9a121b524ca9d5f0b716cf450026891
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-06-09 10:34:44 +0100

    Revert "PODS_5606 Remove debug code as would breach gdpr"
    This reverts commit 1b9c3e6543d183cce80fb673e67f90a616e152b9.

--- app/connectors/cache/UserAnswersCacheConnector.scala
+++ app/connectors/cache/UserAnswersCacheConnector.scala
@@ -18,6 +18,8 @@ package connectors.cache
 
 import com.google.inject.Inject
 import config.FrontendAppConfig
+import play.api.Logger
+import models.{LockDetail, SessionData, SessionAccessData}
 import play.api.http.Status._
 import play.api.libs.json._
 import play.api.libs.ws.WSClient
@@ -26,12 +28,14 @@ import play.api.mvc.Results._
 import uk.gov.hmrc.crypto.PlainText
 import uk.gov.hmrc.http.{HttpException, HeaderCarrier}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Future, ExecutionContext}
 
 class UserAnswersCacheConnectorImpl @Inject()(
                                                config: FrontendAppConfig,
                                                http: WSClient
                                              ) extends UserAnswersCacheConnector {
+
+  private val logger = Logger(classOf[UserAnswersCacheConnectorImpl])
 
   override protected def saveUrl = s"${config.aftUrl}/pension-scheme-accounting-for-tax/journey-cache/aft"
 
@@ -77,6 +81,9 @@ class UserAnswersCacheConnectorImpl @Inject()(
           case CREATED =>
             Future.successful(value)
           case _ =>
+            logger.warn(s"Response status of ${response.status} received from " +
+              s"posting to $url. Response body is ${response.body}. " +
+              s"The original value posted was $value.")
             Future.failed(new HttpException(response.body, response.status))
         }
       }
