9027668bd3a8d7bb1d65822577b7c355034e7394
bonnie.brown <bonnie.brown@digital.hmrc.gov.uk>
2021-12-08 13:48:35 +0000

    PODS-6204 WIP Not Submission Time for AFT page created

---
+++ app/controllers/NotSubmissionTimeController.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import controllers.actions.IdentifierAction
+import renderer.Renderer
+import play.api.mvc.Results.Ok
+import java.time.LocalDate
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class NotSubmissionTimeController @Inject()(renderer: Renderer,
+                                            identify: IdentifierAction
+                                           )(implicit val executionContext: ExecutionContext) {
+
+  def onPageLoad(srn: String) = {
+    identify.async {
+      implicit request =>
+        renderer.render("notSubmissionTime.njk").map(Ok(_))
+    }
+  }
+}
