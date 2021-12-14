10f287b5b46a0fce77e071d294242c0110719424
bonnie.brown <bonnie.brown@digital.hmrc.gov.uk>
2021-12-14 10:03:44 +0000

    PODS-6204 WIP Added Unit Tests

--- test/navigators/ChargeNavigatorSpec.scala
+++ test/navigators/ChargeNavigatorSpec.scala
@@ -136,7 +136,7 @@ class ChargeNavigatorSpec extends NavigatorBehaviour {
           version = 2),
 
         rowWithDateAndVersion(AFTSummaryPage)(
-          controllers.routes.ReturnToSchemeDetailsController.returnToSchemeDetails(srn, startDate, accessType, versionInt),
+          controllers.routes.NotSubmissionTimeController.onPageLoad(srn, startDate),
           aftSummaryNo(quarter = SampleData.q32020),
           currentDate = SampleData.q32020.endDate,
           version = 1)
