36786606eca5da6498d2874162b7b7789ed5803d
al-kouhestani <al.kouhestani@accenture.com>
2022-01-17 11:56:04 +0000

    changes

---
+++ test/controllers/fileUpload/ValidationControllerSpec.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.fileUpload
+
+import connectors.UpscanInitiateConnector
+import controllers.actions.MutableFakeDataRetrievalAction
+import controllers.base.ControllerSpecBase
+import data.SampleData._
+import fileUploadParsers.{AnnualAllowanceParser, ParserValidationErrors, ValidationResult}
+import matchers.JsonMatchers
+import models.{ChargeType, UploadId, UpscanFileReference, UpscanInitiateResponse, UserAnswers}
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.libs.json.{JsObject, Json}
+import play.api.test.Helpers.{route, status, _}
+import play.twirl.api.Html
+import uk.gov.hmrc.viewmodels.NunjucksSupport
+
+import scala.concurrent.Future
+
+class ValidationControllerSpec extends ControllerSpecBase with NunjucksSupport with JsonMatchers {
+  private val mutableFakeDataRetrievalAction: MutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
+  private val templateToBeRendered = "fileUpload/invalid.njk"
+  private val chargeType = ChargeType.ChargeTypeAnnualAllowance
+
+  private def ua: UserAnswers = userAnswersWithSchemeName
+
+  val expectedJson: JsObject = Json.obj()
+
+  private val mockUpscanInitiateConnector: UpscanInitiateConnector = mock[UpscanInitiateConnector]
+
+
+
+  val extraModules: Seq[GuiceableModule] = Seq[GuiceableModule](
+    bind[UpscanInitiateConnector].toInstance(mockUpscanInitiateConnector)
+  )
+
+  private val formFieldsMap = Map(
+    "testField1" -> "value1",
+    "testField2" -> "value2"
+  )
+
+  private val upscanInitiateResponse = UpscanInitiateResponse(
+    fileReference = UpscanFileReference(""),
+    postTarget = "/postTarget",
+    formFields = formFieldsMap
+  )
+
+  private val maxUploadFileSize = 99
+
+  override def beforeEach: Unit = {
+    super.beforeEach
+    reset(mockUpscanInitiateConnector, mockAppConfig, mockRenderer)
+    when(mockRenderer.render(any(), any())(any())).thenReturn(Future.successful(Html("")))
+    when(mockAppConfig.maxUploadFileSize).thenReturn(maxUploadFileSize)
+  }
+
+  private val application: Application = applicationBuilderMutableRetrievalAction(mutableFakeDataRetrievalAction, extraModules).build()
+
+  "onPageLoad" must {
+    "return OK and the correct view for a GET" in {
+      when(mockUpscanInitiateConnector.initiateV2(any(), any())(any())).thenReturn(Future.successful(upscanInitiateResponse))
+
+      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
+
+      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
+      val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
+      val errors : List[ParserValidationErrors]= List(
+        ParserValidationErrors (10, Seq("haha"))
+      )
+      val validationResult = ValidationResult(UserAnswers(), errors)
+      val mockAnnualAllowanceParser = mock[AnnualAllowanceParser]
+      when(mockAnnualAllowanceParser.parse(any(),any()))
+        .thenReturn(validationResult)
+      val result = route(application,
+        httpGETRequest(controllers.fileUpload.routes.ValidationController.onPageLoad(srn, startDate, accessType, versionInt, chargeType, UploadId("")).url)).value
+
+      status(result) mustEqual OK
+
+      verify(mockRenderer, times(1)).render(templateCaptor.capture(), jsonCaptor.capture())(any())
+
+      templateCaptor.getValue mustEqual templateToBeRendered
+      val jsonToPassToTemplate = Json.obj(
+        "chargeType" -> chargeType.toString,
+        "chargeTypeText" -> chargeType.toString,
+        "srn" -> srn,
+        "viewModel" -> errors
+      )
+
+      jsonCaptor.getValue must containJson(jsonToPassToTemplate)
+    }
+  }
+
+  
+  
+}
