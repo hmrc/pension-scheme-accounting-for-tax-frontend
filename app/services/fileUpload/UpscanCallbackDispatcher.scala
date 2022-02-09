d130d0329705a40a03d2d10c7dd986441e303258
jigneshkumarpatel <86969870+jigneshkumarpatel@users.noreply.github.com>
2022-02-09 13:26:14 +0000

    PODS:callback url changes

--- app/services/fileUpload/UpscanCallbackDispatcher.scala
+++ app/services/fileUpload/UpscanCallbackDispatcher.scala
@@ -16,9 +16,9 @@
 
 package services.fileUpload
 
-import controllers.fileUpload.{CallbackBody, FailedCallbackBody, ReadyCallbackBody}
 import models._
 import uk.gov.hmrc.http.HeaderCarrier
+import upscan.callback.{CallbackBody, FailedCallbackBody, ReadyCallbackBody}
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
