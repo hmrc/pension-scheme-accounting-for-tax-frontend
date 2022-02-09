6ba021dd4b0e166df7f730952835f874891751ae
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-02-09 13:48:00 +0000

    PODS-6443 WIP

--- test/controllers/fileUpload/ValidationControllerSpec.scala
+++ test/controllers/fileUpload/ValidationControllerSpec.scala
@@ -19,13 +19,25 @@ package controllers.fileUpload
 import connectors.{Reference, UpscanInitiateConnector}
 import controllers.actions.MutableFakeDataRetrievalAction
 import controllers.base.ControllerSpecBase
+import data.SampleData
 import data.SampleData._
 import fileUploadParsers.{AnnualAllowanceParser, ParserValidationError}
 import matchers.JsonMatchers
 import models.LocalDateBinder._
+import models.SponsoringEmployerType.SponsoringEmployerTypeIndividual
+import models.chargeA.{ChargeDetails => ChargeADetails}
+import models.chargeB.ChargeBDetails
+import models.chargeF.{ChargeDetails => ChargeFDetails}
 import models.{ChargeType, UploadId, UploadStatus, UploadedSuccessfully, UserAnswers}
 import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
+import pages.chargeA.{ChargeDetailsPage => ChargeADetailsPage}
+import pages.chargeB.ChargeBDetailsPage
+import pages.chargeC.{ChargeCDetailsPage, SponsoringIndividualDetailsPage, WhichTypeOfSponsoringEmployerPage}
+import pages.chargeD.{ChargeDetailsPage => ChargeDDetailsPage, MemberDetailsPage => MemberDDetailsPage}
+import pages.chargeE.{ChargeDetailsPage => ChargeEDetailsPage, MemberDetailsPage => MemberEDetailsPage}
+import pages.chargeF.{ChargeDetailsPage => ChargeFDetailsPage}
+import pages.chargeG.{ChargeAmountsPage, MemberDetailsPage => MemberGDetailsPage}
 import play.api.Application
 import play.api.inject.bind
 import play.api.inject.guice.GuiceableModule
@@ -37,41 +49,13 @@ import services.fileUpload.{FileUploadAftReturnService, UploadProgressTracker}
 import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
 import uk.gov.hmrc.viewmodels.NunjucksSupport
 
+import java.time.LocalDate
 import scala.concurrent.{ExecutionContext, Future}
 
 class ValidationControllerSpec extends ControllerSpecBase with NunjucksSupport with JsonMatchers {
-  private val mutableFakeDataRetrievalAction: MutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
-  private val templateToBeRendered = "fileUpload/invalid.njk"
-  private val chargeType = ChargeType.ChargeTypeAnnualAllowance
-  private def ua: UserAnswers = userAnswersWithSchemeName
 
-  val expectedJson: JsObject = Json.obj()
+  import ValidationControllerSpec._
 
-  private val mockUpscanInitiateConnector: UpscanInitiateConnector = mock[UpscanInitiateConnector]
-  private val mockAFTService: AFTService = mock[AFTService]
-  private val mockFileUploadAftReturnService: FileUploadAftReturnService = mock[FileUploadAftReturnService]
-
-
-  private val fakeUploadProgressTracker: UploadProgressTracker = new UploadProgressTracker {
-    override def requestUpload(uploadId: UploadId, fileReference: Reference)(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Unit] =
-      Future.successful(())
-
-    override def registerUploadResult(reference: Reference, uploadStatus: UploadStatus)(implicit ec: ExecutionContext,
-                                                                                        hc: HeaderCarrier): Future[Unit] = Future.successful(())
-
-    override def getUploadResult(id: UploadId)(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Option[UploadStatus]] =
-      Future.successful(
-        Some(
-          UploadedSuccessfully(
-            name = "name",
-            mimeType = "mime",
-            downloadUrl = "/test",
-            size = Some(1L)
-          )))
-  }
-
-  private val mockAnnualAllowanceParser = mock[AnnualAllowanceParser]
-  private val template = "fileUpload/fileUploadSuccess.njk"
   private val extraModules: Seq[GuiceableModule] = Seq[GuiceableModule](
     bind[UpscanInitiateConnector].toInstance(mockUpscanInitiateConnector),
     bind[AnnualAllowanceParser].toInstance(mockAnnualAllowanceParser),
@@ -159,19 +143,17 @@ class ValidationControllerSpec extends ControllerSpecBase with NunjucksSupport w
 
     "redirect OK to the next page and save items to be committed into the Mongo database when there are no validation errors" in {
       val jsonCaptor = ArgumentCaptor.forClass(classOf[JsObject])
-      val templateCaptor = ArgumentCaptor.forClass(classOf[String])
       when(mockUserAnswersCacheConnector.save(any(), jsonCaptor.capture())(any(), any()))
         .thenReturn(Future.successful(JsNull))
       mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
 
       val ci = UserAnswers()
-        .setOrException( JsPath \ "testNode1", JsString("test1"))
-        .setOrException( JsPath \ "testNode2", JsString("test2"))
-
+        .setOrException(JsPath \ "testNode1", JsString("test1"))
+        .setOrException(JsPath \ "testNode2", JsString("test2"))
 
       when(mockAnnualAllowanceParser.parse(any(), any(), any())(any())).thenReturn(Right(ci))
       when(mockFileUploadAftReturnService.preProcessAftReturn(any(), any())(any(), any(), any())).thenReturn(Future.successful(ci))
-      when(mockAFTService.fileCompileReturn(any(), any())(any(), any(), any())).thenReturn(Future.successful())
+      when(mockAFTService.fileCompileReturn(any(), any())(any(), any(), any())).thenReturn(Future.successful(()))
       when(mockCompoundNavigator.nextPage(any(), any(), any(), any(), any(), any(), any())(any())).thenReturn(dummyCall)
       val result = route(
         application,
@@ -180,10 +162,55 @@ class ValidationControllerSpec extends ControllerSpecBase with NunjucksSupport w
       ).value
 
       status(result) mustEqual SEE_OTHER
-      
+
       redirectLocation(result) mustBe Some(routes.FileUploadSuccessController.onPageLoad(srn, startDate, accessType, versionInt, chargeType).url)
 
     }
   }
 
+}
+
+object ValidationControllerSpec extends ControllerSpecBase with NunjucksSupport with JsonMatchers {
+  private val mutableFakeDataRetrievalAction: MutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
+  private val templateToBeRendered = "fileUpload/invalid.njk"
+  private val chargeType = ChargeType.ChargeTypeAnnualAllowance
+
+  private def ua: UserAnswers = userAnswersWithSchemeName
+
+  private val mockUpscanInitiateConnector: UpscanInitiateConnector = mock[UpscanInitiateConnector]
+  private val mockAFTService: AFTService = mock[AFTService]
+  private val mockFileUploadAftReturnService: FileUploadAftReturnService = mock[FileUploadAftReturnService]
+
+  private val fakeUploadProgressTracker: UploadProgressTracker = new UploadProgressTracker {
+    override def requestUpload(uploadId: UploadId, fileReference: Reference)(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Unit] =
+      Future.successful(())
+
+    override def registerUploadResult(reference: Reference, uploadStatus: UploadStatus)(implicit ec: ExecutionContext,
+                                                                                        hc: HeaderCarrier): Future[Unit] = Future.successful(())
+
+    override def getUploadResult(id: UploadId)(implicit ec: ExecutionContext, hc: HeaderCarrier): Future[Option[UploadStatus]] =
+      Future.successful(
+        Some(
+          UploadedSuccessfully(
+            name = "name",
+            mimeType = "mime",
+            downloadUrl = "/test",
+            size = Some(1L)
+          )))
+  }
+
+  private val mockAnnualAllowanceParser = mock[AnnualAllowanceParser]
+  val uaAllChargeTypes: UserAnswers = UserAnswers()
+    .setOrException(ChargeADetailsPage, ChargeADetails(2, Some(200.00), Some(200.00), 400.00))
+    .setOrException(ChargeBDetailsPage, ChargeBDetails(2, 400.00))
+    .setOrException(WhichTypeOfSponsoringEmployerPage(0), SponsoringEmployerTypeIndividual)
+    .setOrException(SponsoringIndividualDetailsPage(0), SampleData.sponsoringIndividualDetails)
+    .setOrException(ChargeCDetailsPage(0), SampleData.chargeCDetails)
+    .setOrException(MemberDDetailsPage(0), SampleData.memberDetails)
+    .setOrException(ChargeDDetailsPage(0), SampleData.chargeDDetails)
+    .setOrException(MemberEDetailsPage(0), SampleData.memberDetails)
+    .setOrException(ChargeEDetailsPage(0), SampleData.chargeEDetails)
+    .setOrException(ChargeFDetailsPage, ChargeFDetails(LocalDate.now(), 400.00))
+    .setOrException(MemberGDetailsPage(0), SampleData.memberGDetails)
+    .setOrException(ChargeAmountsPage(0), SampleData.chargeAmounts)
 }
