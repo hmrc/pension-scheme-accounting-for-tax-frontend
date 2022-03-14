160370dcec496717f785e1df7b9e3fbe2e6aa69d
mazin ghafadgi <mazin_ghafadgi@yahoo.co.uk>
2022-03-14 16:30:14 +0000

    Fixed LifeTimeAllownce tests

--- test/fileUploadParsers/LifetimeAllowanceParserSpec.scala
+++ test/fileUploadParsers/LifetimeAllowanceParserSpec.scala
@@ -1,4 +1,4 @@
-/*
+
 /*
  * Copyright 2022 HM Revenue & Customs
  *
@@ -46,8 +46,14 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
 
   "LifeTime allowance parser" must {
     "return charges in user answers when there are no validation errors" in {
+      val GivingValidCSVFile = CsvParser.split(
+        s"""$header
+                            Joe,Bloggs,AB123456C,01/04/2020,268.28,0.00
+                            Joe,Bliggs,AB123457C,01/04/2020,268.28,0.00"""
+      )
+
       val chargeDetails = ChargeDDetails(LocalDate.of(2020, 4, 1), Some(BigDecimal(268.28)), None)
-      val result = parser.parse(startDate, validCsvFile, UserAnswers())
+      val result = parser.parse(startDate, GivingValidCSVFile, UserAnswers())
       result mustBe Right(UserAnswers()
         .setOrException(MemberDetailsPage(0).path, Json.toJson(SampleData.memberDetails2))
         .setOrException(ChargeDetailsPage(0).path, Json.toJson(chargeDetails))
@@ -57,7 +63,8 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation error for incorrect header" in {
-      val result = parser.parse(startDate, Seq("test"),UserAnswers())
+      val GivingIncorrectHeader = CsvParser.split("""test""")
+      val result = parser.parse(startDate, GivingIncorrectHeader,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(0, 0, "Header invalid or File is empty")
       ))
@@ -71,14 +78,23 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation error for not enough fields" in {
-      val result = parser.parse(startDate, Seq(header, "one,two"),UserAnswers())
+      val GivingNotEnoughFields = CsvParser.split(
+        s"""$header
+                            one,two"""
+      )
+      val result = parser.parse(startDate,GivingNotEnoughFields,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(1, 0, "Not enough fields")
       ))
     }
 
     "return validation errors for member details when present" in {
-      val result = parser.parse(startDate, invalidMemberDetailsCsvFile,UserAnswers())
+      val GivingIncorrectMemberDetails = CsvParser.split(
+        s"""$header
+                            ,Bloggs,AB123456C,01/04/2020,268.28,0.00
+                            Ann,,3456C,01/04/2020,268.28,0.00"""
+      )
+      val result = parser.parse(startDate, GivingIncorrectMemberDetails,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(1, 0, "memberDetails.error.firstName.required", "firstName"),
         ParserValidationError(2, 1, "memberDetails.error.lastName.required", "lastName"),
@@ -87,7 +103,13 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation errors for charge details when present, including missing year and missing month" in {
-      val result = parser.parse(startDate, invalidChargeDetailsCsvFile,UserAnswers())
+      val GivingMissingYearAndMonth = CsvParser.split(
+        s"""$header
+                            Joe,Bloggs,AB123456C,01/04,268.28,0.00
+                            Ann,Bliggs,AB123457C,01,268.28,0.00"""
+      )
+
+      val result = parser.parse(startDate, GivingMissingYearAndMonth,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(1, 3, "dateOfEvent.error.incomplete", "dateOfEvent",Seq("year")),
         ParserValidationError(2, 3, "dateOfEvent.error.incomplete", "dateOfEvent",Seq("month","year"))
@@ -95,7 +117,12 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation errors for member details AND charge details when both present" in {
-      val result = parser.parse(startDate, invalidMemberDetailsAndChargeDetailsCsvFile,UserAnswers())
+      val GivingIncorrectMemberDetailsAndChargeDetails = CsvParser.split(
+        s"""$header
+                            ,Bloggs,AB123456C,01/04,268.28,0.00
+                            Ann,,3456C,01,268.28,0.00"""
+      )
+      val result = parser.parse(startDate, GivingIncorrectMemberDetailsAndChargeDetails,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(1, 0, "memberDetails.error.firstName.required", "firstName"),
         ParserValidationError(1, 3, "dateOfEvent.error.incomplete", "dateOfEvent",Seq("year")),
@@ -106,7 +133,13 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation errors for member details AND charge details when errors present in first row but not in second" in {
-      val result = parser.parse(startDate, invalidMemberDetailsAndChargeDetailsFirstRowCsvFile,UserAnswers())
+      val GivingIncorrectMemberDetailsAndChargeDetailsFirstRow = CsvParser.split(
+        s"""$header
+                            ,Bloggs,AB123456C,01/04,268.28,0.00
+                            Joe,Bliggs,AB123457C,01/04/2020,268.28,0.00"""
+      )
+
+      val result = parser.parse(startDate, GivingIncorrectMemberDetailsAndChargeDetailsFirstRow,UserAnswers())
       result mustBe Left(Seq(
         ParserValidationError(1, 0, "memberDetails.error.firstName.required", "firstName"),
         ParserValidationError(1, 3, "dateOfEvent.error.incomplete", "dateOfEvent",Seq("year")),
@@ -114,7 +147,11 @@ class LifetimeAllowanceParserSpec extends SpecBase with Matchers with MockitoSug
     }
 
     "return validation errors when not enough fields" in {
-      val result = parser.parse(startDate, Seq(header, "Bloggs,AB123456C,2020268.28,2020-01-01,true"),UserAnswers())
+      val GivingNotEnoughFields = CsvParser.split(
+        s"""$header
+                            Bloggs,AB123456C,2020268.28,2020-01-01,true"""
+      )
+      val result = parser.parse(startDate, GivingNotEnoughFields,UserAnswers())
 
       result mustBe Left(Seq(ParserValidationError(1, 0, "Not enough fields")))
     }
@@ -158,4 +195,4 @@ object LifetimeAllowanceParserSpec {
   private val chargeDetailsFormProvider = new ChargeDetailsFormProvider
 
   private val parser = new LifetimeAllowanceParser(memberDetailsFormProvider, chargeDetailsFormProvider, mockFrontendAppConfig)
-}*/
+}
