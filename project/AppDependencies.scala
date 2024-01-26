4afe888c0f22e3f8ce0ce821ee06be02587e7f57
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-01-26 14:44:27 +0000

    PODS-9042 - Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"                   %%  "play-nunjucks"                          % "0.41.0-play-28",
     "uk.gov.hmrc"                   %%  "play-nunjucks-viewmodel"                % "0.17.0-play-28",
-    "org.webjars.npm"               %   "govuk-frontend"                         % "4.3.1",
+    "org.webjars.npm"               %   "govuk-frontend"                         % "4.7.0",
     "org.webjars.npm"               %   "hmrc-frontend"                          % "1.35.2",
     "com.google.inject.extensions"  %   "guice-multibindings"                    % "4.2.3",
     "uk.gov.hmrc"                   %%  "domain"                                 % "8.3.0-play-28",
@@ -19,12 +19,12 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "org.scalatest"               %% "scalatest"                  % "3.2.14",
+    "org.scalatest"               %% "scalatest"                  % "3.2.15",
     "org.scalatestplus.play"      %% "scalatestplus-play"         % "5.1.0",
-    "org.scalatestplus"           %% "mockito-4-6"                % "3.2.14.0",
-    "org.scalatestplus"           %% "scalacheck-1-17"            % "3.2.14.0",
+    "org.scalatestplus"           %% "mockito-4-6"                % "3.2.15.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"            % "3.2.15.0",
     "org.pegdown"                 %  "pegdown"                    % "1.6.0",
-    "com.vladsch.flexmark"        %  "flexmark-all"               % "0.62.2"
+    "com.vladsch.flexmark"        %  "flexmark-all"               % "0.64.6"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
