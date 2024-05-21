228f1e1a3a083b14dc2f9f5b8a9809867c68f17d
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2024-05-21 14:45:10 +0100

    PODS-9330: View now dynamically creates links
    Code:
    - View now dynamically loads the content and sections as required

--- app/controllers/AFTOverviewController.scala
+++ app/controllers/AFTOverviewController.scala
@@ -17,18 +17,21 @@
 package controllers
 
 import config.FrontendAppConfig
-import controllers.AFTOverviewController.{OverviewViewModel, outstandingAmountStr}
+import controllers.AFTOverviewController._
 import controllers.actions.{AllowAccessActionProviderForIdentifierRequest, IdentifierAction}
+import models.AFTQuarter.formatForDisplay
+import models.financialStatement.PaymentOrChargeType.AccountingForTaxCharges
+import models.{AFTQuarter, DisplayQuarter, SchemeDetails}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.{JsObject, Json, OWrites}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import renderer.Renderer
-import services.SchemeService
+import services.{QuartersService, SchemeService}
 import uk.gov.hmrc.nunjucks.NunjucksSupport
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class AFTOverviewController @Inject()(
                                        identify: IdentifierAction,
@@ -36,46 +39,96 @@ class AFTOverviewController @Inject()(
                                        allowAccess: AllowAccessActionProviderForIdentifierRequest,
                                        config: FrontendAppConfig,
                                        schemeService: SchemeService,
+                                       quartersService: QuartersService,
                                        override val messagesApi: MessagesApi,
                                        val controllerComponents: MessagesControllerComponents
-)(implicit ec: ExecutionContext)
-    extends FrontendBaseController
-    with I18nSupport
-    with NunjucksSupport {
+                                     )(implicit ec: ExecutionContext)
+  extends FrontendBaseController with I18nSupport with NunjucksSupport {
 
+//scalastyle:off
   def onPageLoad(srn: String): Action[AnyContent] = (identify andThen allowAccess(Some(srn))).async {
     implicit request =>
-      schemeService.retrieveSchemeDetails(
-        psaId = request.idOrException,
-        schemeIdType = "srn",
-        srn = srn
-      ).flatMap { sD =>
+
+      (for {
+        schemeDetails <- schemeService.retrieveSchemeDetails(
+          psaId = request.idOrException,
+          schemeIdType = schemeIdType,
+          srn = srn
+        )
+
+        quartersInProgress <- quartersService.getInProgressQuarters(
+          srn = srn,
+          pstr = schemeDetails.pstr
+        )
+
+        allPastYears <- quartersService.getPastYears(
+          pstr = schemeDetails.pstr
+        )
+
+        pastYearsAndQuarters <- Future.traverse(displayYears(allPastYears))(
+          year => quartersService.getPastQuarters(
+              pstr = schemeDetails.pstr,
+              year = year
+            ).flatMap(quarters => Future.successful(year, quarters))
+        )
+
+      } yield OverviewInfo(schemeDetails, quartersInProgress, pastYearsAndQuarters)
+        ).flatMap { overviewInfo =>
 
         // TODO: remove hardcoded value
         val outstandingAmount: BigDecimal = 123.45
 
-        val json: JsObject = Json.obj("viewModel" -> OverviewViewModel(
+        val json: JsObject = Json.obj(
+          viewModel -> OverviewViewModel(
             returnUrl = config.schemeDashboardUrl(request).format(srn),
-            schemeName = sD.schemeName,
-            outstandingAmount = outstandingAmountStr(outstandingAmount)
+            schemeName = overviewInfo.schemeDetails.schemeName,
+            outstandingAmount = outstandingAmountStr(outstandingAmount),
+            quartersInProgress = overviewInfo.quartersInProgress.map(qIP =>
+              (formatForDisplay(qIP.quarter), linkForQuarter(srn, qIP.quarter))
+            ),
+            pastYearsAndQuarters = overviewInfo.pastYearsAndQuarters.map(pYAQ =>
+              (pYAQ._1,
+                pYAQ._2.map(q =>
+                  (formatForDisplay(q.quarter), linkForQuarter(srn, q.quarter))
+                )
+              )
+            )
           )
         )
-        renderer.render("aftOverview.njk", json).map(Ok(_))
+        renderer.render(template, json).map(Ok(_))
       }
   }
 }
 
 object AFTOverviewController {
 
+  private val schemeIdType = "srn"
+  private val viewModel = "viewModel"
+  private val template = "aftOverview.njk"
+  private val maxPastYearsToDisplay = 3
+
   // TODO - remove "hardcoded" from string
   private val outstandingAmountStr: BigDecimal => String = amount => s"£$amount - hardcoded"
+
+  private val linkForQuarter: (String, AFTQuarter) => String = (srn, aftQuarter) =>
+    controllers.financialOverview.scheme.routes.AllPaymentsAndChargesController
+      .onPageLoad(srn, aftQuarter.startDate.toString, AccountingForTaxCharges).url
+
+  private val displayYears: Seq[Int] => Seq[Int] = seq => seq.sorted.reverse.take(maxPastYearsToDisplay)
+  private case class OverviewInfo(
+                           schemeDetails: SchemeDetails,
+                           quartersInProgress: Seq[DisplayQuarter],
+                           pastYearsAndQuarters: Seq[(Int, Seq[DisplayQuarter])]
+                         )
   case class OverviewViewModel(
                                 returnUrl: String,
                                 schemeName: String,
-                                outstandingAmount: String
+                                outstandingAmount: String,
+                                quartersInProgress: Seq[(String, String)] = Seq.empty,
+                                pastYearsAndQuarters: Seq[(Int, Seq[(String, String)])] = Seq.empty
                               )
 
-  object OverviewViewModel {
+  private object OverviewViewModel {
     implicit lazy val writes: OWrites[OverviewViewModel] = Json.writes[OverviewViewModel]
   }
 }
