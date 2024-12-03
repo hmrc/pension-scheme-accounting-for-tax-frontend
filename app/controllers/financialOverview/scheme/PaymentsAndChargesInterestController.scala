2eed971ceb6e021072c537f002ebaf23640335e8
jamdade-sharad <sharad.jamdade@digital.hmrc.gov.uk>
2024-12-03 14:14:51 +0000

    PODS-10050 Scheme overdue charges details amendments

--- app/controllers/financialOverview/scheme/PaymentsAndChargesInterestController.scala
+++ app/controllers/financialOverview/scheme/PaymentsAndChargesInterestController.scala
@@ -37,6 +37,7 @@ import uk.gov.hmrc.viewmodels.SummaryList.{Key, Row, Value}
 import uk.gov.hmrc.viewmodels.Text.Literal
 import uk.gov.hmrc.viewmodels.{Html, NunjucksSupport, SummaryList}
 import utils.DateHelper
+import utils.DateHelper.formatDateDMY
 
 import java.time.LocalDate
 import javax.inject.Inject
@@ -92,10 +93,18 @@ class PaymentsAndChargesInterestController @Inject()(
       case Some(schemeFs) =>
         val originalAmountUrl = routes.PaymentsAndChargeDetailsController.onPageLoad(srn, period, index,
           paymentOrChargeType, version, submittedDate, journeyType).url
-        renderer.render(
-          template = "financialOverview/scheme/paymentsAndChargeInterest.njk",
-          ctx = summaryListData(srn, request.psaId, request.pspId, schemeFs, schemeName, originalAmountUrl, version, journeyType)
-        ).map(Ok(_))
+
+        if(config.podsNewFinancialCredits) {
+          renderer.render(
+            template = "financialOverview/scheme/paymentsAndChargeInterestNew.njk",
+            ctx = summaryListDataV2(srn, request.psaId, request.pspId, schemeFs, schemeName, originalAmountUrl, version, journeyType)
+          ).map(Ok(_))
+        } else {
+          renderer.render(
+            template = "financialOverview/scheme/paymentsAndChargeInterest.njk",
+            ctx = summaryListData(srn, request.psaId, request.pspId, schemeFs, schemeName, originalAmountUrl, version, journeyType)
+          ).map(Ok(_))
+        }
       case _ =>
         logger.warn(s"Scheme not found for index $index")
         Future.successful(Redirect(controllers.routes.SessionExpiredController.onPageLoad))
@@ -125,6 +134,37 @@ class PaymentsAndChargesInterestController @Inject()(
         case (None, interestChargeTypeText) =>
           interestChargeTypeText
           }
+        ),
+      "insetText" -> htmlInsetText,
+      "originalAmountUrl" -> originalAmountUrl,
+      "returnLinkBasedOnJourney" -> paymentsAndChargesService.getReturnLinkBasedOnJourney(journeyType, schemeName),
+      "returnUrl" -> paymentsAndChargesService.getReturnUrl(srn, psaId, pspId, config, journeyType)
+    )
+  }
+
+  private def summaryListDataV2(srn: String, psaId: Option[PsaId], pspId: Option[PspId], schemeFSDetail: SchemeFSDetail, schemeName: String,
+                              originalAmountUrl: String, version: Option[Int], journeyType: ChargeDetailsFilter)
+                             (implicit messages: Messages): JsObject = {
+
+    val htmlInsetText =
+      Html(
+        s"<p class=govuk-body>${messages("paymentsAndCharges.interest.chargeReference.text1")}" +
+          s" <span><a id='breakdown' class=govuk-link href=$originalAmountUrl>" +
+          s" ${messages("paymentsAndCharges.interest.chargeReference.linkText")}</a></span>" +
+          s" ${messages("paymentsAndCharges.interest.chargeReference.text2")}</p>"
+      )
+    val interestChargeType = getInterestChargeTypeText(schemeFSDetail.chargeType)
+    Json.obj(
+      fields = "chargeDetailsList" -> getSummaryListRowsV2(schemeFSDetail),
+      "schemeName" -> schemeName,
+      "interestDueAmount" -> FormatHelper.formatCurrencyAmountAsString(schemeFSDetail.accruedInterestTotal),
+      "accruedInterest" -> schemeFSDetail.accruedInterestTotal,
+      "chargeType" -> ((version, interestChargeType) match {
+        case (Some(value), interestChargeTypeText) =>
+          interestChargeTypeText + s" submission $value"
+        case (None, interestChargeTypeText) =>
+          interestChargeTypeText
+      }
         ),
       "insetText" -> htmlInsetText,
       "originalAmountUrl" -> originalAmountUrl,
@@ -163,6 +203,33 @@ class PaymentsAndChargesInterestController @Inject()(
         value = Value(
           Literal(s"${FormatHelper.formatCurrencyAmountAsString(schemeFSDetail.accruedInterestTotal)}"),
           classes = Seq("govuk-!-width-one-quarter", "govuk-!-font-weight-bold")
+        ),
+        actions = Nil
+      ))
+  }
+
+  private def getSummaryListRowsV2(schemeFSDetail: SchemeFSDetail): Seq[SummaryList.Row] = {
+    Seq(
+      Row(
+        key = Key(
+          content = msg"financialPaymentsAndCharges.chargeReference",
+          classes = Seq("govuk-!-padding-left-0", "govuk-!-width-one-quarter")
+        ),
+        value = Value(
+          content = msg"paymentsAndCharges.chargeReference.toBeAssigned",
+          classes =
+            Seq("govuk-!-width-one-quarter")
+        ),
+        actions = Nil
+      ),
+      Row(
+        key = Key(
+          msg"pension.scheme.interest.tax.period.new",
+          classes = Seq("govuk-!-padding-left-0", "govuk-!-width-three-quarters", "govuk-!-font-weight-bold")
+        ),
+        value = Value(
+          Literal(s"${formatDateDMY(schemeFSDetail.periodStartDate) + " to " + formatDateDMY(schemeFSDetail.periodEndDate)}"),
+          classes = Seq("govuk-!-width-one-quarter")
         ),
         actions = Nil
       ))
