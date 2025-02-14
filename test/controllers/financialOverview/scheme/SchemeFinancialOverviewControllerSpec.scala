8aa7bd1b20b3f5949a4b87e5bce6cc5b5f7bd1c2
nonebula <147166151+nonebula@users.noreply.github.com>
2025-02-14 13:30:53 +0000

    PODS-10367 - remove incorrect test placeholders

--- test/controllers/financialOverview/scheme/SchemeFinancialOverviewControllerSpec.scala
+++ test/controllers/financialOverview/scheme/SchemeFinancialOverviewControllerSpec.scala
@@ -98,8 +98,6 @@ class SchemeFinancialOverviewControllerSpec
           .thenReturn(allTypesMultipleReturnsModel)
         when(mockFinancialStatementConnector.getSchemeFSPaymentOnAccount(any())(any(), any()))
           .thenReturn(Future.successful(schemeFSResponseAftAndOTC))
-        when(mockPsaSchemePartialService.creditBalanceAmountFormatted(any()))
-          .thenReturn("£1,000.00")
         when(mockMinimalPsaConnector.getPsaOrPspName(any(), any(), any()))
           .thenReturn(Future.successful("John Doe"))
         when(mockAppConfig.podsNewFinancialCredits).thenReturn(true)
@@ -141,8 +139,6 @@ class SchemeFinancialOverviewControllerSpec
           .thenReturn(allTypesMultipleReturnsModel)
         when(mockFinancialStatementConnector.getSchemeFSPaymentOnAccount(any())(any(), any()))
           .thenReturn(Future.successful(schemeFSResponseAftAndOTC))
-        when(mockPsaSchemePartialService.creditBalanceAmountFormatted(any()))
-          .thenReturn("£1,000.00")
         when(mockMinimalPsaConnector.getPsaOrPspName(any(), any(), any()))
           .thenReturn(Future.successful("John Doe"))
         when(mockAppConfig.podsNewFinancialCredits).thenReturn(false)
@@ -163,10 +159,6 @@ class SchemeFinancialOverviewControllerSpec
           allPaymentLink = routes.PaymentOrChargeTypeController.onPageLoad(srn).url,
           creditBalanceFormatted = "£0.00",
           creditBalance = 0,
-          displayReceivedPayments = ???,
-          receivedPaymentsLink = ???,
-          displayHistory = ???,
-          historyLink = ???,
           isOverdueChargeAvailable = false,
           returnUrl = mockAppConfig.managePensionsSchemeOverviewUrl
         )(messages, request)
