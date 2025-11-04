822990cce4dc3db271daec3d26d1be9f57632998
Vincent Vinnicombe <5462800+vinnicombe@users.noreply.github.com>
2025-11-04 09:35:45 +0000

    DDCNL-11260: Updated bootstrap to 10.4.0 and removed _timeout-dialog.scss in favour of hmrc-frontend provided import

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,12 +1,12 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "10.2.0"
+  private val bootstrapVersion = "10.4.0"
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %%  "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-30"             % bootstrapVersion,
-    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"             % "12.17.0",
+    "uk.gov.hmrc"                   %%  "play-frontend-hmrc-play-30"             % "12.19.0",
     "com.google.inject.extensions"  %   "guice-multibindings"                    % "4.2.3",
     "uk.gov.hmrc"                   %%  "domain-play-30"                         % "13.0.0",
     "com.univocity"                 %   "univocity-parsers"                      % "2.9.1",
